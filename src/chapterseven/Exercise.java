971a10b6f374bfae76d24cdaed64cc773e92ded9
cyl <1194923550@qq.com>
2023-12-04 20:56:54 +0800

    chapter7

--- src/chapterseven/Exercise.java
+++ src/chapterseven/Exercise.java
@@ -24,6 +24,7 @@ public class Exercise {
         }
     }
 }
+
 class Tom {
     public int tomNum;
     public int comNum;
@@ -34,11 +35,13 @@ class Tom {
         Random ran = new Random();
         comNum = ran.nextInt(3);
     }
+
     public void setTomNum(int tomNum) {
         this.tomNum = tomNum;
     }
+
     public void compare() {
-        if ((tomNum == 0 && comNum == 1) || (tomNum == 1 && comNum == 2) ||(tomNum == 2 && comNum == 0)) {
+        if ((tomNum == 0 && comNum == 1) || (tomNum == 1 && comNum == 2) || (tomNum == 2 && comNum == 0)) {
             System.out.println("你赢了");
         } else if (tomNum == comNum) {
             System.out.println("平了");
@@ -47,19 +50,23 @@ class Tom {
         }
     }
 }
+
 class Circle {
     public double radius;
 
     Circle(double radius) {
         this.radius = radius;
     }
+
     Circle() {
 
     }
+
     public double findArea() {
         return Math.PI * radius * radius;
     }
 }
+
 class PassObject {
     public void printArea(Circle c, int times) {
         System.out.println("Radius\tArea");
@@ -69,6 +76,7 @@ class PassObject {
         }
     }
 }
+
 class A01 {
     public Double max(double[] nums) {
         if (nums.length == 0) {
@@ -97,14 +105,16 @@ class A02 {
 
 class Book {
     int price;
+
     public void updatePrice() {
-        if (this.price > 150){
+        if (this.price > 150) {
             this.price = 150;
         } else if (this.price > 100) {
             this.price = 100;
         }
     }
 }
+
 class Employee {
     public String name;
     public char sex;
@@ -117,10 +127,12 @@ class Employee {
         this.age = age;
         this.sex = sex;
     }
+
     Employee(String level, double salary) {
         this.level = level;
         this.salary = salary;
     }
+
     Employee(String name, char sex, int age, String level, double salary) {
         this(name, sex, age);
 //        this(level, salary);
